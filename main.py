a4bbd49c6d5a0059df1809eeced5bf3a1ddbbd63
fastfruits <nichoias.henry1006@gmail.com>
2024-10-22 20:20:40 -0400

    ggf

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-import requests
 import json
 import ui
-from datetime import date
 import threading
 
-print(str(date.today()))
-
-def getToken(file_path, variable):
-    with open(file_path, 'r') as file:
-        config = json.load(file)
-        return config.get(variable)
 
 def mainFunction():
-    while ui.news == "":
-        print("here")
-
     url = "https://api.worldnewsapi.com/search-news?text=earth+quake&language=en&earliest-publish-date=" + str(date.today()) + ""
     api_key = getToken('config.json', 'newsToken')
 
---
+++ worldnews.py
@@ -0,0 +1,46 @@
+import worldnewsapi
+from worldnewsapi.rest import ApiException
+from datetime import date, timedelta
+import ui
+
+def getToken(file_path, variable):
+    with open(file_path, 'r') as file:
+        config = json.load(file)
+        return config.get(variable)
+
+newsapi_key = getToken('config.json', 'newsToken')
+newsapi_configuration = worldnewapi.Configuration(api_key={'apiKey': newsapi_key})
+newsType = ui.news
+
+
+def mainFunction():
+    try:
+        newsapiInstance = worldnewsapi.NewsApi(worldnewsapi.ApiClient(newsapi_configuration))
+
+        response = newsapiInstance.search_news(
+            text="",
+            source_countries='us',
+            language='en',
+            earliest_publish_date=str(date.today()-timedelta(days=31)),
+            latest_publish_date=str(date.today()),
+            categories=newsType,
+            sort="publish-time",
+            sort_direction="desc",
+            min_sentiment=0,
+            max_sentiment=0.75,
+            offset=0,
+            number=5)
+
+    except ApiException as e:
+        print("Exception when calling news API\n" % e)
+
+    f = open("txtReturn.txt", "x")
+    for articles in news.news:
+        f.write(str(articles.text))
+        print("Title: " + str(article.title));
+        print("Text: " + str(article.text));
+    f.close()
+
+
+if __name__ == "__main__":
+    print(mainFunction())
